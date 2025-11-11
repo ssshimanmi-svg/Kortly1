c1198b6e3c7287b39d9acd478f22c53cec4205cf
ssshimanmi-svg <ssshimanmi@gmail.com>
2025-11-11 21:03:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -654,7 +654,7 @@ const filtered = useMemo(() => {
 {/* ===== ПАНЕЛЬ ФИЛЬТРОВ ===== */}
 <section className="border-b border-neutral-900">
   <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-6">
-    <div className="grid gap-4 sm:grid-cols-4 lg:grid-cols-6">
+    <div className="grid gap-8 sm:grid-cols-4 lg:grid-cols-6">
       {/* поиск */}
       <div className="sm:col-span-2">
         <label className="text-sm text-neutral-400">Поиск по названию или адресу</label>
